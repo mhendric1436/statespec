20fecba2125adc1665957e2438ca055e3ffb8a6a
Mark Hendrickson <hendrickson.mark.l@gmail.com>
2026-05-08 15:00:51 -0600

    Expand parser interface for core syntax

--- include/statespec/parser.hpp
+++ include/statespec/parser.hpp
@@ -4,6 +4,7 @@
 #include "statespec/diagnostic.hpp"
 #include "statespec/token.hpp"
 
+#include <string>
 #include <vector>
 
 namespace statespec
@@ -17,7 +18,36 @@ class Parser
     Spec parse(DiagnosticBag& diagnostics);
 
   private:
+    const Token& peek(std::size_t offset = 0) const;
+    const Token& previous() const;
+    bool is_at_end() const;
+    bool check(TokenKind kind) const;
+    bool check_any(std::initializer_list<TokenKind> kinds) const;
+    bool match(TokenKind kind);
+    bool match_any(std::initializer_list<TokenKind> kinds);
+    Token advance();
+    Token consume(TokenKind kind, const std::string& message, DiagnosticBag& diagnostics);
+
+    Spec parse_spec(DiagnosticBag& diagnostics);
+    ImportDecl parse_import_decl(DiagnosticBag& diagnostics);
+    SystemDecl parse_system_decl(DiagnosticBag& diagnostics);
+    EntityDecl parse_entity_decl(DiagnosticBag& diagnostics);
+    WorkflowDecl parse_workflow_decl(DiagnosticBag& diagnostics);
+    WorkflowStepDecl parse_workflow_step_decl(DiagnosticBag& diagnostics);
+    StateMachineDecl parse_state_machine_decl(DiagnosticBag& diagnostics);
+    FieldDecl parse_field_decl(DiagnosticBag& diagnostics);
+
+    std::string parse_qualified_name(DiagnosticBag& diagnostics, const std::string& context);
+    std::string parse_type_name(DiagnosticBag& diagnostics);
+    std::vector<std::string> parse_identifier_list(DiagnosticBag& diagnostics);
+
+    void consume_optional_semicolon();
+    void skip_unknown_declaration(DiagnosticBag& diagnostics);
+    void skip_balanced_block();
+    void synchronize_to_member_boundary();
+
     std::vector<Token> tokens_;
+    std::size_t current_ = 0;
 };
 
 } // namespace statespec
