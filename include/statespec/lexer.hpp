e628190d444faa0ca3d5d484ed887501726b32ed
Mark Hendrickson <hendrickson.mark.l@gmail.com>
2026-05-08 14:37:55 -0600

    Add StateSpec lexer skeleton

---
+++ include/statespec/lexer.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "statespec/diagnostic.hpp"
+#include "statespec/source.hpp"
+#include "statespec/token.hpp"
+
+#include <vector>
+
+namespace statespec {
+
+class Lexer {
+public:
+    explicit Lexer(const SourceFile& source);
+
+    std::vector<Token> lex(DiagnosticBag& diagnostics);
+
+private:
+    const SourceFile& source_;
+};
+
+} // namespace statespec
