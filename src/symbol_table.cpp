6155b37135ffb211fa1aa548934ea6a5c59bf3aa
Mark Hendrickson <hendrickson.mark.l@gmail.com>
2026-05-08 14:40:55 -0600

    Add StateSpec symbol table implementation

---
+++ src/symbol_table.cpp
@@ -0,0 +1,23 @@
+#include "statespec/symbol_table.hpp"
+
+#include <utility>
+
+namespace statespec {
+
+bool SymbolTable::insert(Symbol symbol)
+{
+    const auto name = symbol.name;
+    return symbols_.emplace(name, std::move(symbol)).second;
+}
+
+std::optional<Symbol> SymbolTable::find(const std::string& name) const
+{
+    const auto it = symbols_.find(name);
+    if (it == symbols_.end())
+    {
+        return std::nullopt;
+    }
+    return it->second;
+}
+
+} // namespace statespec
