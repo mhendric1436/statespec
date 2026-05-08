e8fa26de0db4c596b0e7151e4c577a3d3917eb8f
Mark Hendrickson <hendrickson.mark.l@gmail.com>
2026-05-08 14:39:42 -0600

    Add StateSpec source implementation

---
+++ src/source.cpp
@@ -0,0 +1,22 @@
+#include "statespec/source.hpp"
+
+#include <utility>
+
+namespace statespec {
+
+SourceFile::SourceFile(std::string path, std::string text)
+    : path_(std::move(path)), text_(std::move(text))
+{
+}
+
+const std::string& SourceFile::path() const noexcept
+{
+    return path_;
+}
+
+const std::string& SourceFile::text() const noexcept
+{
+    return text_;
+}
+
+} // namespace statespec
