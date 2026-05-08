6b4b3152e61b419c9b05fbe62cb21e52f8502139
Mark Hendrickson <hendrickson.mark.l@gmail.com>
2026-05-08 14:40:19 -0600

    Add StateSpec AST implementation placeholder

---
+++ src/ast.cpp
@@ -0,0 +1,8 @@
+#include "statespec/ast.hpp"
+
+namespace statespec {
+
+// AST nodes are currently simple value types. This translation unit exists so
+// build systems can link a stable statespec library as the AST grows.
+
+} // namespace statespec
