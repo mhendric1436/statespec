22a1452025642e2b38b7e47757ec4fdd1c60a449
Mark Hendrickson <hendrickson.mark.l@gmail.com>
2026-05-08 14:39:13 -0600

    Add StateSpec validator skeleton

---
+++ include/statespec/validator.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "statespec/ast.hpp"
+#include "statespec/diagnostic.hpp"
+
+namespace statespec {
+
+class Validator {
+public:
+    void validate(const Spec& spec, DiagnosticBag& diagnostics);
+};
+
+} // namespace statespec
