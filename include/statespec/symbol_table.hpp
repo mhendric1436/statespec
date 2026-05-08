c1cdcec45d4ac7d5abd3c84c796cd09c3ee9b73d
Mark Hendrickson <hendrickson.mark.l@gmail.com>
2026-05-08 14:38:50 -0600

    Add StateSpec symbol table skeleton

---
+++ include/statespec/symbol_table.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "statespec/source.hpp"
+
+#include <optional>
+#include <string>
+#include <unordered_map>
+
+namespace statespec {
+
+enum class SymbolKind {
+    System,
+    Namespace,
+    Value,
+    Enum,
+    Shape,
+    Entity,
+    Event,
+    Queue,
+    Message,
+    Lease,
+    Worker,
+    Api,
+    Workflow,
+    WorkflowStep,
+    Policy,
+    GenerateTarget,
+};
+
+struct Symbol {
+    SymbolKind kind = SymbolKind::System;
+    std::string name;
+    SourceRange range;
+};
+
+class SymbolTable {
+public:
+    bool insert(Symbol symbol);
+    std::optional<Symbol> find(const std::string& name) const;
+
+private:
+    std::unordered_map<std::string, Symbol> symbols_;
+};
+
+} // namespace statespec
