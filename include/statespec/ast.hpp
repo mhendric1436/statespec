0a462a545c9bba55f85d8f613b6eb00e25615f6f
Mark Hendrickson <hendrickson.mark.l@gmail.com>
2026-05-08 14:38:17 -0600

    Add StateSpec AST skeleton

---
+++ include/statespec/ast.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "statespec/source.hpp"
+
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace statespec {
+
+struct ImportDecl {
+    std::string name;
+    std::optional<std::string> alias;
+    SourceRange range;
+};
+
+struct SystemDecl {
+    std::string name;
+    SourceRange range;
+};
+
+struct Spec {
+    std::optional<std::string> version;
+    std::vector<ImportDecl> imports;
+    std::optional<SystemDecl> system;
+};
+
+} // namespace statespec
