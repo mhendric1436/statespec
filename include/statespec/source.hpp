32b62f9c8b0c1c64b5db334cf5acbb26017208b8
Mark Hendrickson <hendrickson.mark.l@gmail.com>
2026-05-08 14:37:20 -0600

    Add StateSpec C++ parser skeleton source header

---
+++ include/statespec/source.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+
+namespace statespec {
+
+struct SourceLocation {
+    std::size_t offset = 0;
+    std::size_t line = 1;
+    std::size_t column = 1;
+};
+
+struct SourceRange {
+    SourceLocation begin;
+    SourceLocation end;
+};
+
+class SourceFile {
+public:
+    SourceFile(std::string path, std::string text);
+
+    const std::string& path() const noexcept;
+    const std::string& text() const noexcept;
+
+private:
+    std::string path_;
+    std::string text_;
+};
+
+} // namespace statespec
