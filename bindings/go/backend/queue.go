95ab933594e842b0043ba3ac5aa181d07a8c326f
Mark Hendrickson <hendrickson.mark.l@gmail.com>
2026-05-10 20:09:20 -0600

    refactor: collapse Go queue runtime into backend-passing store API

--- bindings/go/backend/queue.go
+++ bindings/go/backend/queue.go
@@ -65,154 +65,17 @@ type FailMessageRequest struct {
 }
 
 type QueueStore interface {
-	Create(ctx context.Context, tx Transaction, request CreateQueueRequest) (QueueCreation, error)
+	Create(ctx context.Context, backend Backend, request CreateQueueRequest) (QueueCreation, error)
 
-	InspectDefinition(ctx context.Context, tx Transaction, queue string, channel string) (*QueueDefinition, error)
+	InspectDefinition(ctx context.Context, backend Backend, queue string, channel string) (*QueueDefinition, error)
 
-	Enqueue(ctx context.Context, tx Transaction, request EnqueueMessageRequest) (QueueMessageRecord, error)
+	Enqueue(ctx context.Context, backend Backend, request EnqueueMessageRequest) (QueueMessageRecord, error)
 
-	Claim(ctx context.Context, tx Transaction, request ClaimMessageRequest) ([]QueueMessageRecord, error)
+	Claim(ctx context.Context, backend Backend, request ClaimMessageRequest) ([]QueueMessageRecord, error)
 
-	Acknowledge(ctx context.Context, tx Transaction, request AckMessageRequest) error
+	Acknowledge(ctx context.Context, backend Backend, request AckMessageRequest) error
 
-	Fail(ctx context.Context, tx Transaction, request FailMessageRequest) (QueueMessageRecord, error)
+	Fail(ctx context.Context, backend Backend, request FailMessageRequest) (QueueMessageRecord, error)
 
-	Inspect(ctx context.Context, tx Transaction, messageID string) (*QueueMessageRecord, error)
-}
-
-type QueueRuntime struct {
-	Backend Backend
-	Store   QueueStore
-}
-
-func NewQueueRuntime(backend Backend, store QueueStore) QueueRuntime {
-	return QueueRuntime{Backend: backend, Store: store}
-}
-
-func (r QueueRuntime) Create(ctx context.Context, request CreateQueueRequest) (QueueCreation, error) {
-	tx, err := r.Backend.Begin(ctx)
-	if err != nil {
-		return QueueCreation{}, err
-	}
-
-	result, err := r.Store.Create(ctx, tx, request)
-	if err != nil {
-		_ = tx.Abort(ctx)
-		return QueueCreation{}, err
-	}
-
-	if err := r.Backend.Commit(ctx, tx); err != nil {
-		return QueueCreation{}, err
-	}
-
-	return result, nil
-}
-
-func (r QueueRuntime) InspectDefinition(ctx context.Context, queue string, channel string) (*QueueDefinition, error) {
-	tx, err := r.Backend.Begin(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	result, err := r.Store.InspectDefinition(ctx, tx, queue, channel)
-	if err != nil {
-		_ = tx.Abort(ctx)
-		return nil, err
-	}
-
-	if err := r.Backend.Commit(ctx, tx); err != nil {
-		return nil, err
-	}
-
-	return result, nil
-}
-
-func (r QueueRuntime) Enqueue(ctx context.Context, request EnqueueMessageRequest) (QueueMessageRecord, error) {
-	tx, err := r.Backend.Begin(ctx)
-	if err != nil {
-		return QueueMessageRecord{}, err
-	}
-
-	result, err := r.Store.Enqueue(ctx, tx, request)
-	if err != nil {
-		_ = tx.Abort(ctx)
-		return QueueMessageRecord{}, err
-	}
-
-	if err := r.Backend.Commit(ctx, tx); err != nil {
-		return QueueMessageRecord{}, err
-	}
-
-	return result, nil
-}
-
-func (r QueueRuntime) Claim(ctx context.Context, request ClaimMessageRequest) ([]QueueMessageRecord, error) {
-	tx, err := r.Backend.Begin(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	result, err := r.Store.Claim(ctx, tx, request)
-	if err != nil {
-		_ = tx.Abort(ctx)
-		return nil, err
-	}
-
-	if err := r.Backend.Commit(ctx, tx); err != nil {
-		return nil, err
-	}
-
-	return result, nil
-}
-
-func (r QueueRuntime) Acknowledge(ctx context.Context, request AckMessageRequest) error {
-	tx, err := r.Backend.Begin(ctx)
-	if err != nil {
-		return err
-	}
-
-	if err := r.Store.Acknowledge(ctx, tx, request); err != nil {
-		_ = tx.Abort(ctx)
-		return err
-	}
-
-	return r.Backend.Commit(ctx, tx)
-}
-
-func (r QueueRuntime) Fail(ctx context.Context, request FailMessageRequest) (QueueMessageRecord, error) {
-	tx, err := r.Backend.Begin(ctx)
-	if err != nil {
-		return QueueMessageRecord{}, err
-	}
-
-	result, err := r.Store.Fail(ctx, tx, request)
-	if err != nil {
-		_ = tx.Abort(ctx)
-		return QueueMessageRecord{}, err
-	}
-
-	if err := r.Backend.Commit(ctx, tx); err != nil {
-		return QueueMessageRecord{}, err
-	}
-
-	return result, nil
-}
-
-func (r QueueRuntime) Inspect(ctx context.Context, messageID string) (*QueueMessageRecord, error) {
-	tx, err := r.Backend.Begin(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	result, err := r.Store.Inspect(ctx, tx, messageID)
-	if err != nil {
-		_ = tx.Abort(ctx)
-		return nil, err
-	}
-
-	if err := r.Backend.Commit(ctx, tx); err != nil {
-		return nil, err
-	}
-
-	return result, nil
+	Inspect(ctx context.Context, backend Backend, messageID string) (*QueueMessageRecord, error)
 }
