568c3f52f29ef112c612020d865165f102384afa
Mark Hendrickson <hendrickson.mark.l@gmail.com>
2026-05-11 12:17:19 -0700

    feat: add Rust lease transaction-passing methods

--- bindings/rust/lease.rs
+++ bindings/rust/lease.rs
@@ -1,6 +1,6 @@
 use std::time::{Duration, SystemTime};
 
-use crate::backend::{Backend, BackendResult};
+use crate::backend::{Backend, BackendResult, Transaction};
 
 #[derive(Debug, Clone)]
 pub struct LeaseRecord {
@@ -43,9 +43,17 @@ pub struct LeaseAcquireResult {
 pub trait LeaseStore<B: Backend> {
     fn acquire(&self, backend: &B, request: &LeaseAcquireRequest) -> BackendResult<LeaseAcquireResult>;
 
+    fn acquire_tx(&self, tx: &mut B::Tx, request: &LeaseAcquireRequest) -> BackendResult<LeaseAcquireResult>;
+
     fn renew(&self, backend: &B, request: &LeaseRenewRequest) -> BackendResult<LeaseRecord>;
+
+    fn renew_tx(&self, tx: &mut B::Tx, request: &LeaseRenewRequest) -> BackendResult<LeaseRecord>;
 
     fn release(&self, backend: &B, request: &LeaseReleaseRequest) -> BackendResult<()>;
 
+    fn release_tx(&self, tx: &mut B::Tx, request: &LeaseReleaseRequest) -> BackendResult<()>;
+
     fn inspect(&self, backend: &B, resource: &str) -> BackendResult<Option<LeaseRecord>>;
+
+    fn inspect_tx(&self, tx: &mut B::Tx, resource: &str) -> BackendResult<Option<LeaseRecord>>;
 }
