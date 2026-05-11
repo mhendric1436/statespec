fc6f3cb33b1ef5ad317242fe6b053f9e587e2725
Mark Hendrickson <hendrickson.mark.l@gmail.com>
2026-05-11 12:17:53 -0700

    feat: add Rust queue transaction-passing methods

--- bindings/rust/queue.rs
+++ bindings/rust/queue.rs
@@ -72,6 +72,8 @@ pub struct FailMessageRequest {
 pub trait QueueStore<B: Backend> {
     fn create(&self, backend: &B, request: &CreateQueueRequest) -> BackendResult<QueueCreation>;
 
+    fn create_tx(&self, tx: &mut B::Tx, request: &CreateQueueRequest) -> BackendResult<QueueCreation>;
+
     fn inspect_definition(
         &self,
         backend: &B,
@@ -79,13 +81,30 @@ pub trait QueueStore<B: Backend> {
         channel: &str,
     ) -> BackendResult<Option<QueueDefinition>>;
 
+    fn inspect_definition_tx(
+        &self,
+        tx: &mut B::Tx,
+        queue: &str,
+        channel: &str,
+    ) -> BackendResult<Option<QueueDefinition>>;
+
     fn enqueue(&self, backend: &B, request: &EnqueueMessageRequest) -> BackendResult<QueueMessageRecord>;
+
+    fn enqueue_tx(&self, tx: &mut B::Tx, request: &EnqueueMessageRequest) -> BackendResult<QueueMessageRecord>;
 
     fn claim(&self, backend: &B, request: &ClaimMessageRequest) -> BackendResult<Vec<QueueMessageRecord>>;
 
+    fn claim_tx(&self, tx: &mut B::Tx, request: &ClaimMessageRequest) -> BackendResult<Vec<QueueMessageRecord>>;
+
     fn acknowledge(&self, backend: &B, request: &AckMessageRequest) -> BackendResult<()>;
+
+    fn acknowledge_tx(&self, tx: &mut B::Tx, request: &AckMessageRequest) -> BackendResult<()>;
 
     fn fail(&self, backend: &B, request: &FailMessageRequest) -> BackendResult<QueueMessageRecord>;
 
+    fn fail_tx(&self, tx: &mut B::Tx, request: &FailMessageRequest) -> BackendResult<QueueMessageRecord>;
+
     fn inspect(&self, backend: &B, message_id: &str) -> BackendResult<Option<QueueMessageRecord>>;
+
+    fn inspect_tx(&self, tx: &mut B::Tx, message_id: &str) -> BackendResult<Option<QueueMessageRecord>>;
 }
