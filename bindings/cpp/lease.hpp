7a9ee2c0d1e2255504ae64f6040902aa119b0ba6
Mark Hendrickson <hendrickson.mark.l@gmail.com>
2026-05-12 19:07:51 -0700

    Make C++ leases definition-first

--- bindings/cpp/lease.hpp
+++ bindings/cpp/lease.hpp
@@ -12,8 +12,31 @@ namespace statespec::backend
 
 using Timestamp = std::chrono::system_clock::time_point;
 
+struct LeaseDefinitionId
+{
+    std::string name;
+    std::uint64_t version = 1;
+};
+
+struct LeaseDefinition
+{
+    LeaseDefinitionId id;
+    std::string resource_pattern;
+    std::chrono::seconds ttl;
+    std::chrono::seconds renew_every;
+    std::optional<std::chrono::seconds> max_ttl;
+    bool fencing_token = true;
+};
+
+struct LeaseRegisterDefinitionResult
+{
+    bool registered_new = false;
+    LeaseDefinition definition;
+};
+
 struct LeaseRecord
 {
+    LeaseDefinitionId definition_id;
     std::string resource;
     std::optional<std::string> holder;
     Timestamp expires_at;
@@ -22,26 +45,33 @@ struct LeaseRecord
 
 struct LeaseAcquireRequest
 {
+    LeaseDefinitionId definition_id;
     std::string resource;
     std::string holder;
     Timestamp now;
-    std::chrono::seconds ttl;
 };
 
 struct LeaseRenewRequest
 {
+    LeaseDefinitionId definition_id;
     std::string resource;
     std::string holder;
     std::uint64_t fencing_token = 0;
     Timestamp now;
-    std::chrono::seconds ttl;
 };
 
 struct LeaseReleaseRequest
 {
+    LeaseDefinitionId definition_id;
     std::string resource;
     std::string holder;
     std::uint64_t fencing_token = 0;
+};
+
+struct LeaseInspectRequest
+{
+    LeaseDefinitionId definition_id;
+    std::string resource;
 };
 
 struct LeaseAcquireResult
@@ -54,6 +84,26 @@ class ILeaseStore
 {
   public:
     virtual ~ILeaseStore() = default;
+
+    virtual LeaseRegisterDefinitionResult register_definition(
+        IBackend& backend,
+        const LeaseDefinition& definition
+    ) = 0;
+
+    virtual LeaseRegisterDefinitionResult register_definitionTx(
+        ITransaction& tx,
+        const LeaseDefinition& definition
+    ) = 0;
+
+    virtual std::optional<LeaseDefinition> inspect_definition(
+        IBackend& backend,
+        const LeaseDefinitionId& definition_id
+    ) = 0;
+
+    virtual std::optional<LeaseDefinition> inspect_definitionTx(
+        ITransaction& tx,
+        const LeaseDefinitionId& definition_id
+    ) = 0;
 
     virtual LeaseAcquireResult acquire(
         IBackend& backend,
@@ -87,12 +137,12 @@ class ILeaseStore
 
     virtual std::optional<LeaseRecord> inspect(
         IBackend& backend,
-        const std::string& resource
+        const LeaseInspectRequest& request
     ) = 0;
 
     virtual std::optional<LeaseRecord> inspectTx(
         ITransaction& tx,
-        const std::string& resource
+        const LeaseInspectRequest& request
     ) = 0;
 };
 
