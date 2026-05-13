f45441e2f88f8f1f05e2e4379e7b4ec8c174ff3d
Mark Hendrickson <hendrickson.mark.l@gmail.com>
2026-05-12 19:08:04 -0700

    Make Go leases definition-first

--- bindings/go/backend/lease.go
+++ bindings/go/backend/lease.go
@@ -5,7 +5,27 @@ import (
 	"time"
 )
 
+type LeaseDefinitionID struct {
+	Name    string
+	Version uint64
+}
+
+type LeaseDefinition struct {
+	ID              LeaseDefinitionID
+	ResourcePattern string
+	TTL             time.Duration
+	RenewEvery      time.Duration
+	MaxTTL          *time.Duration
+	FencingToken    bool
+}
+
+type LeaseRegisterDefinitionResult struct {
+	RegisteredNew bool
+	Definition    LeaseDefinition
+}
+
 type LeaseRecord struct {
+	DefinitionID LeaseDefinitionID
 	Resource     string
 	Holder       *string
 	ExpiresAt    time.Time
@@ -13,24 +33,30 @@ type LeaseRecord struct {
 }
 
 type LeaseAcquireRequest struct {
-	Resource string
-	Holder   string
-	Now      time.Time
-	TTL      time.Duration
+	DefinitionID LeaseDefinitionID
+	Resource     string
+	Holder       string
+	Now          time.Time
 }
 
 type LeaseRenewRequest struct {
+	DefinitionID LeaseDefinitionID
 	Resource     string
 	Holder       string
 	FencingToken uint64
 	Now          time.Time
-	TTL          time.Duration
 }
 
 type LeaseReleaseRequest struct {
+	DefinitionID LeaseDefinitionID
 	Resource     string
 	Holder       string
 	FencingToken uint64
+}
+
+type LeaseInspectRequest struct {
+	DefinitionID LeaseDefinitionID
+	Resource     string
 }
 
 type LeaseAcquireResult struct {
@@ -39,6 +65,14 @@ type LeaseAcquireResult struct {
 }
 
 type LeaseStore interface {
+	RegisterDefinition(ctx context.Context, backend Backend, definition LeaseDefinition) (LeaseRegisterDefinitionResult, error)
+
+	RegisterDefinitionTx(ctx context.Context, tx Transaction, definition LeaseDefinition) (LeaseRegisterDefinitionResult, error)
+
+	InspectDefinition(ctx context.Context, backend Backend, definitionID LeaseDefinitionID) (*LeaseDefinition, error)
+
+	InspectDefinitionTx(ctx context.Context, tx Transaction, definitionID LeaseDefinitionID) (*LeaseDefinition, error)
+
 	Acquire(ctx context.Context, backend Backend, request LeaseAcquireRequest) (LeaseAcquireResult, error)
 
 	AcquireTx(ctx context.Context, tx Transaction, request LeaseAcquireRequest) (LeaseAcquireResult, error)
@@ -51,7 +85,7 @@ type LeaseStore interface {
 
 	ReleaseTx(ctx context.Context, tx Transaction, request LeaseReleaseRequest) error
 
-	Inspect(ctx context.Context, backend Backend, resource string) (*LeaseRecord, error)
+	Inspect(ctx context.Context, backend Backend, request LeaseInspectRequest) (*LeaseRecord, error)
 
-	InspectTx(ctx context.Context, tx Transaction, resource string) (*LeaseRecord, error)
+	InspectTx(ctx context.Context, tx Transaction, request LeaseInspectRequest) (*LeaseRecord, error)
 }
