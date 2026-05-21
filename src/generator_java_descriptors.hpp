ed4fd2fd3ad96c88755abf2cccf89d0656cb5764
Mark Hendrickson <hendrickson.mark.l@gmail.com>
2026-05-20 21:45:55 -0600

    refactor: move Java descriptor generation into focused module

---
+++ src/generator_java_descriptors.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "statespec/generator_bindings.hpp"
+
+#include <string>
+
+namespace statespec
+{
+
+std::string generate_descriptors_java(const IrSystem& system);
+
+std::string generate_workflow_step_handler_keys_java(const IrSystem& system);
+
+} // namespace statespec
